85ab0d129716d8b58775646078c3d403bbde16c3
MatiasRZ7 <matiaz777@hotmail.com>
2024-02-23 15:40:56 +0100

    4 commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,39 @@
-import { ShoppingCart } from 'phosphor-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './navbar.css' 
+import { ShoppingCart } from 'phosphor-react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
 export const Navbar = () => {
+  const [show, setShow] = useState(true);
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (scrollPos > currentScrollPos) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+
+      setScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollPos]);
+
   return (
-    <div className='navbar'>
-        <div className='links'>
+    <div className='navbar' style={{ top: show ? '0' : '-100px', transition: 'top 0.3s ease-out' }}>
+      <div className='links'>
         <Link to={"/"}>Shop</Link>
         <Link to={"cart"}>
-        <ShoppingCart size={32}/>
+          <ShoppingCart size={32}/>
         </Link>
-        
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
